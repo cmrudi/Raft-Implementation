b0cac6cddc5bde2fd235332b9a562046e3266d3c
cmrudi <cutmeurahrudi@gmail.com>
2017-04-25 01:53:09 +0700

    load balancer, add initialization

--- LoadBalancer/loadBalancerAPI.py
+++ LoadBalancer/loadBalancerAPI.py
@@ -1,13 +1,41 @@
-from bottle import route, run
+from bottle import route, run, request
+import requests
+import sys
+import os
 
-@route('/api/search_prime/:name')
-def index(name):
-    return '<b>Hello %s!</b>' % name
+
+localhost = ''
+leader_host = ''
+
+@route('/api/search_prime/:prime_nth')
+def index(prime_nth):
+    r = requests.get('https://github.com/cmrudi')
+    return '<b>Hello %s!</b>' % localhost
     
 @route('/api/catch_request/:name')
 def index(name):
     return '<b>Hellosss %s!</b>' % name
 
-run(host='localhost', port=5000)
+@route('/api/join_api/:ip_addr')
+def index(name):
+    return '<b>Hello %s!</b>' % ip_addr
 
 
+
+
+def initialize():
+    global localhost
+    global leader_host
+    shell_response = os.popen('ifconfig wlp2s0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
+    localhost = shell_response.read()
+    localhost = localhost[:-1]
+    leader_host = sys.argv[1]  
+    if (str(localhost) == str(leader_host)):
+        print('Initiate Leader')
+    else:
+		response = requests.get('https://'+ leader_host +'/api/join_system/'+localhost)
+		print(response);
+
+if __name__ == '__main__':
+    initialize()
+    run(host='localhost', port=5000)
