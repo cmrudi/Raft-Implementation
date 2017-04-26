16087110fba31e5086877ceb8a06ce0a6bdc2f41
Dyas <dyas@live.com>
2017-04-26 21:28:03 +0700

    log

--- strukturDataLog.py
+++ strukturDataLog.py
@@ -1,29 +1,31 @@
 class Log_element:
-	def __init__(self, num, com):
-		self.num = num # number
+	def __init__(self, ip, term, com):
+		self.ip = ip # ip address
+		self.term = term # term
 		self.com = com # commited
+
 class Log:
 	def __init__(self):
 		self.log_elem = []
 	
 	def print_log(self):
-	# print everything in the log
+	# print everything in the log (untuk debugging)
 		for obj in self.log_elem:
-			print obj.num, obj.com
+			print obj.ip, obj.term, obj.com
 
-	def add(self,new_number):
+	def add(self,new_ip, term):
 	# adding a number request
-		self.log_elem.append(Log_element(new_number,False))
+		self.log_elem.append(Log_element(new_ip, term,False))
 
 	def commit(self):
 	# commit the last value
 		self.log_elem[len(self.log_elem)-1].com = True
 
-	def commit_num(self, num_request):
+	def commit_ip_term(self, ip, term):
 	# commit the latest that has num element numRequest
 		i = len(self.log_elem)-1
 		while (i>=0):
-			if ((self.log_elem[i].num == num_request) and (self.log_elem[i].com == False)):
+			if ((self.log_elem[i].ip == ip) and (self.log_elem[i].term == term) and (self.log_elem[i].com == False)):
 				self.log_elem[i].com = True
 				break
 			else:
@@ -33,29 +35,29 @@ class Log:
 	# uncommit the last value
 		self.log_elem[len(self.log_elem)-1].com = False
 
-	def uncommit_num(self, num_request):
+	def uncommit_ip_term(self, ip, term):
 	# uncommit the latest that has num element numRequest
 		i = len(self.log_elem)-1
 		while (i>=0):
-			if ((self.log_elem[i].num == num_request) and (self.log_elem[i].com == True)):
+			if ((self.log_elem[i].ip == ip) and (self.log_elem[i].term == term) and (self.log_elem[i].com == True)):
 				self.log_elem[i].com = False
 				break
 			else:
 				i = i-1
 
-	def check_number(self, num_request):
+	def check_ip(self, ip):
 	# check if there exist the num_request
 		for obj in self.log_elem:
-			if obj.num==num_request:
+			if (obj.ip == ip):
 				return True
 
 		return False
 
-	def get_commit(self, num_request):
+	def get_commit(self, ip, term):
 	# get the commit element of the latest num_request
 		i = len(self.log_elem)-1
 		while (i>=0):
-			if (self.log_elem[i].num == num_request):
+			if (self.log_elem[i].ip == ip) and (self.log_elem[i].term == term):
 				return self.log_elem[i].com
 			else:
 				i = i-1
@@ -63,43 +65,33 @@ class Log:
 
 
 # main (debuging)
-cpu_log = Log()
-cpu_log.add(5)
-cpu_log.commit()
-
 """
-add(log,30)
-add(log,38)
-add(log,18)
-add(log,18)
-add(log,18)
-add(log,20)
-add(log,20)
-add(log,30)
-add(log,38)
-add(log,18)
+log = Log()
+
+log.add("ip1",30)
+log.add("ip2",38)
+log.add("ip3",18)
+log.add("ip2",18)
+log.add("ip1",18)
+log.add("ip4",20)
+log.add("ip5",20)
+log.add("ip6",30)
+log.add("ip7",38)
+log.add("ip8",18)
 # commit(log)
-add(log,20)
-add(log,30)
-add(log,38)
-add(log,1277)
+log.add("ip1",20)
+log.add("ip2",30)
+log.add("ip8",38)
+log.add("ipip",1277)
 
-commit_num(log,18)
-commit_num(log,18)
-commit_num(log,18)
-uncommit_num(log,18)
+log.commit_ip_term("ip1",18)
+log.commit_ip_term("ip3",18)
+log.commit_ip_term("ip",18)
 
-log[1].num = 40
+# log[1].num = 40
 
-print check_number(log,18)
-print get_commit(log,18)
+# print log.check_number(18)
+# print log.get_commit(18)
 
-print_log(log)
-
-print len(log)
-
-x = 17
-y = 25
-z = 0
-print z
+log.print_log()
 """
